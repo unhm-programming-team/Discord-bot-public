b68ae78dcf45b225c0c1e8de48b43a4413883745
klm127 <karlmiller127@gmail.com>
2022-02-25 18:03:03 -0500

    docker containerfile speed improved

--- Containerfile
+++ Containerfile
@@ -1,4 +1,5 @@
 FROM python:3.9
-COPY . /
+COPY ./requirements.txt /requirements.txt
 RUN pip3 install -r /requirements.txt
+COPY . /
 CMD python3 /bot.py
--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from our_packages.key_manager import get_prod
 
 intents = discord.Intents.default()
 intents.members = True
-TESTING = False  # changes weather or not the bot runs on the testing token
+TESTING = True  # changes weather or not the bot runs on the testing token
 CASE_INSENSITIVITY = True  # changes weather or not the bot is insensitive to case
 
 # defines the testing and production token
